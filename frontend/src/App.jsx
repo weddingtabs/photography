753409617190a0f360951e831d7692b2f8040bc3
weddingtabs <weddingtabs360@gmail.com>
2026-09-17 13:38:30 +0530

    Updated top page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,71 @@
+// // import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+// // import TopHeader from './components/TopHeader'
+// // import Footer from './components/Footer'
+// // import WhatsAppButton from './components/WhatsAppButton'
+// // import GetQuoteButton from './components/GetQuoteButton'
+// // import MobileBottomNav from './components/MobileBottomNav'
+// // import ProtectedRoute from './components/ProtectedRoute'
+
+// // import Home from './pages/Home'
+// // import Portfolio from './pages/Portfolio'
+// // import Services from './pages/Services'
+// // import GetQuote from './pages/GetQuote'
+// // import TrackBooking from './pages/TrackBooking'
+// // import About from './pages/About'
+// // import Contact from './pages/Contact'
+// // import AdminLogin from './pages/AdminLogin'
+// // import AdminDashboard from './pages/AdminDashboard'
+
+// // function Layout({ children }) {
+// //   const { pathname } = useLocation()
+// //   const isAdmin = pathname.startsWith('/admin')
+
+// //   return (
+// //     <div className="min-h-screen flex flex-col">
+// //       {!isAdmin && <TopHeader />}
+// //       <main className={`flex-1 ${!isAdmin ? 'pb-16 lg:pb-0' : ''}`}>{children}</main>
+// //       {!isAdmin && <Footer />}
+// //       {!isAdmin && <WhatsAppButton />}
+// //       {!isAdmin && <GetQuoteButton />}
+// //       {!isAdmin && <MobileBottomNav />}
+// //     </div>
+// //   )
+// // }
+
+// // export default function App() {
+// //   return (
+// //     <BrowserRouter>
+// //       <Layout>
+// //         <Routes>
+// //           <Route path="/" element={<Home />} />
+// //           <Route path="/portfolio" element={<Portfolio />} />
+// //           <Route path="/services" element={<Services />} />
+// //           <Route path="/get-quote" element={<GetQuote />} />
+// //           <Route path="/track-booking" element={<TrackBooking />} />
+// //           <Route path="/about" element={<About />} />
+// //           <Route path="/contact" element={<Contact />} />
+
+// //           <Route path="/admin/login" element={<AdminLogin />} />
+// //           <Route
+// //             path="/admin/dashboard"
+// //             element={
+// //               <ProtectedRoute>
+// //                 <AdminDashboard />
+// //               </ProtectedRoute>
+// //             }
+// //           />
+// //         </Routes>
+// //       </Layout>
+// //     </BrowserRouter>
+// //   )
+// // }
+
 // import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 // import TopHeader from './components/TopHeader'
 // import Footer from './components/Footer'
 // import WhatsAppButton from './components/WhatsAppButton'
 // import GetQuoteButton from './components/GetQuoteButton'
+// import CallButton from './components/CallButton'
 // import MobileBottomNav from './components/MobileBottomNav'
 // import ProtectedRoute from './components/ProtectedRoute'
 
@@ -16,17 +79,40 @@
 // import AdminLogin from './pages/AdminLogin'
 // import AdminDashboard from './pages/AdminDashboard'
 
+// // function Layout({ children }) {
+// //   const { pathname } = useLocation()
+// //   const isAdmin = pathname.startsWith('/admin')
+
+// //   return (
+// //     <div className="min-h-screen flex flex-col">
+// //       {!isAdmin && <TopHeader />}
+// //       <main className={`flex-1 ${!isAdmin ? 'pb-16 lg:pb-0' : ''}`}>
+// //         {children}
+// //       </main>
+// //       {!isAdmin && <Footer />}
+// //       {!isAdmin && <WhatsAppButton />}
+// //       {!isAdmin && <GetQuoteButton />}
+// //       {!isAdmin && <CallButton />}
+// //       {!isAdmin && <MobileBottomNav />}
+// //     </div>
+// //   )
+// // }
+
 // function Layout({ children }) {
 //   const { pathname } = useLocation()
 //   const isAdmin = pathname.startsWith('/admin')
+//   const hideFloatingButtons = isAdmin || pathname === '/get-quote'
 
 //   return (
 //     <div className="min-h-screen flex flex-col">
 //       {!isAdmin && <TopHeader />}
-//       <main className={`flex-1 ${!isAdmin ? 'pb-16 lg:pb-0' : ''}`}>{children}</main>
+//       <main className={`flex-1 ${!isAdmin ? 'pb-16 lg:pb-0' : ''}`}>
+//         {children}
+//       </main>
 //       {!isAdmin && <Footer />}
-//       {!isAdmin && <WhatsAppButton />}
-//       {!isAdmin && <GetQuoteButton />}
+//       {!hideFloatingButtons && <WhatsAppButton />}
+//       {!hideFloatingButtons && <GetQuoteButton />}
+//       {!hideFloatingButtons && <CallButton />}
 //       {!isAdmin && <MobileBottomNav />}
 //     </div>
 //   )
@@ -61,6 +147,7 @@
 // }
 
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import TopHeader from './components/TopHeader'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
@@ -78,25 +165,6 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
-
-// function Layout({ children }) {
-//   const { pathname } = useLocation()
-//   const isAdmin = pathname.startsWith('/admin')
-
-//   return (
-//     <div className="min-h-screen flex flex-col">
-//       {!isAdmin && <TopHeader />}
-//       <main className={`flex-1 ${!isAdmin ? 'pb-16 lg:pb-0' : ''}`}>
-//         {children}
-//       </main>
-//       {!isAdmin && <Footer />}
-//       {!isAdmin && <WhatsAppButton />}
-//       {!isAdmin && <GetQuoteButton />}
-//       {!isAdmin && <CallButton />}
-//       {!isAdmin && <MobileBottomNav />}
-//     </div>
-//   )
-// }
 
 function Layout({ children }) {
   const { pathname } = useLocation()
@@ -121,6 +189,7 @@ function Layout({ children }) {
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -145,4 +214,3 @@ export default function App() {
     </BrowserRouter>
   )
 }
-
